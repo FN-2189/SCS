ccf06bf221766c96ef09c1495459df4fb76c55db
Finn <finn294@gmx.de>
2022-07-28 15:26:19 +0200

    change stuff

---
+++ Assets/Control.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Control : MonoBehaviour
+{
+    [SerializeField]
+    private Thrusters thrusters;
+
+    public void OnRoll(InputAction.CallbackContext context)
+    {
+
+    }
+}
