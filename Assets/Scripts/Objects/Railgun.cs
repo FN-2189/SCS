7488373f07fe84eb2f2d1aee1a940d78f940cc10
Julius <jupster18@gmx.de>
2022-09-03 22:36:27 +0200

    add railgun recoil compensation

--- Assets/Scripts/Objects/Railgun.cs
+++ Assets/Scripts/Objects/Railgun.cs
@@ -7,7 +7,9 @@ public class Railgun : MonoBehaviour
 
     [SerializeField] private GameObject pfRailgunShot;
     [SerializeField] ParticleSystem railgunParticles;
+    [SerializeField] ParticleSystem engineParticles;
     [SerializeField] Rigidbody ship;
+    [SerializeField] ThrustManager thrusters;
 
     // Start is called before the first frame update
     void Start()
@@ -24,8 +26,23 @@ public class Railgun : MonoBehaviour
     {
         // + new Vector3(0f, -5.189f, 4.25f)
         railgunParticles.Play();
+
         GameObject g = Instantiate(pfRailgunShot, transform.position, transform.rotation);
         g.GetComponent<Rigidbody>().AddForce(transform.forward * 9980, ForceMode.VelocityChange);
+
         ship.AddForce(transform.forward * -9.98f, ForceMode.Impulse);
+
+        StartCoroutine(RecoilCompensation());
+    }
+
+    private IEnumerator RecoilCompensation()
+    {
+        thrusters.SetManual(Axis.Forward, true);
+        thrusters.SetThrust(Axis.Forward, 1f);
+        engineParticles.startLifetime = 1;
+        yield return new WaitForSeconds(0.01f);
+        engineParticles.startLifetime = 0;
+        thrusters.SetManual(Axis.Forward, false);
+        Debug.Log("Done");
     }
 }
