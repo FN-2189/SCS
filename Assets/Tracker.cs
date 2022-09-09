c3410f93aba2e7cadba7ba1698089474908d7dc9
Finn <finn294@gmx.de>
2022-09-09 19:59:50 +0200

    add missiles

---
+++ Assets/Tracker.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tracker : MonoBehaviour
+{
+    public Transform Target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
