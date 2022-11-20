ae1cec600ff486693d1fdaffe29bfc6edaf1c2e6
Finn <finn294@gmx.de>
2022-11-20 20:22:19 +0100

    add relative velocity

--- Assets/Scripts/Util/RelativeSpace.cs
+++ Assets/Scripts/Util/RelativeSpace.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class RelativeSpace : MonoBehaviour
 {
     private static List<Transform> _allTransforms;
+    private static List<Rigidbody> _allRigidbodies;
+    private static List<Transform> _allTransformsNoRb;
 
     public Transform Player;
+    private Rigidbody _playerRb;
 
     [SerializeField]
     private float maxDistance;
+
+    [SerializeField]
+    private float maxVelocity;
+
+
+    private static Vector3 _currentVelocity = Vector3.zero;
 
 
     // Start is called before the first frame update
@@ -23,13 +32,34 @@ public class RelativeSpace : MonoBehaviour
         {
             _allTransforms.Add(objects[i].transform);
         }
+
+
+        _playerRb = Player.GetComponent<Rigidbody>();
+
+        // get Rigidbodies if present
+        _allRigidbodies = new List<Rigidbody>();
+        // get transforms of objects without rb
+        _allTransformsNoRb = new List<Transform>();
+
+        for(int i = 0; i < objects.Length; i++)
+        {
+            if(objects[i].TryGetComponent<Rigidbody>(out Rigidbody rb))
+            {
+                _allRigidbodies.Add(rb);
+            }
+            else
+            {
+                _allTransformsNoRb.Add(objects[i].transform);
+            }
+        }
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+        #region Position
         // x-axis
-        while(Player.position.x > maxDistance)
+        while (Player.position.x > maxDistance)
         {
             moveObjects(new Vector3(-maxDistance, 0, 0));
         }
@@ -58,6 +88,47 @@ public class RelativeSpace : MonoBehaviour
         {
             moveObjects(new Vector3(0, 0, maxDistance));
         }
+        #endregion
+
+        #region Velocity
+        // x-axis
+        while (_playerRb.velocity.x > maxVelocity)
+        {
+            ChangeVelocities(new Vector3(-maxVelocity, 0, 0));
+        }
+        while (_playerRb.velocity.x < -maxVelocity)
+        {
+            ChangeVelocities(new Vector3(maxVelocity, 0, 0));
+        }
+
+        //y-axis
+        while (_playerRb.velocity.y > maxVelocity)
+        {
+            ChangeVelocities(new Vector3(0, -maxVelocity, 0));
+        }
+        while (_playerRb.velocity.y < -maxVelocity)
+        {
+            ChangeVelocities(new Vector3(0, maxVelocity, 0));
+        }
+
+
+        //z-axis
+        while (_playerRb.velocity.z > maxVelocity)
+        {
+            ChangeVelocities(new Vector3(0, 0, -maxVelocity));
+        }
+        while (_playerRb.velocity.z < -maxVelocity)
+        {
+            ChangeVelocities(new Vector3(0, 0, maxVelocity));
+        }
+        #endregion
+
+
+        // Manually move objects without rb
+        foreach(Transform t in _allTransformsNoRb)
+        {
+            t.position += _currentVelocity * Time.deltaTime;
+        }
     }
 
     private static void moveObjects(Vector3 movement)
@@ -66,15 +137,42 @@ public class RelativeSpace : MonoBehaviour
         {
             t.position += movement;
         }
+
+    }
+
+    private static void ChangeVelocities(Vector3 amount)
+    {
+        foreach (Rigidbody t in _allRigidbodies)
+        {
+            t.velocity += amount;
+        }
+
+        _currentVelocity += amount;
     }
 
     public static void addObject(Transform t)
     {
         _allTransforms.Add(t);
+        if (t.TryGetComponent<Rigidbody>(out Rigidbody rb))
+        {
+            _allRigidbodies.Add(rb);
+        }
+        else
+        {
+            _allTransformsNoRb.Add(t);
+        }
     }
 
     public static void removeObject(Transform t)
     {
         _allTransforms.Remove(t);
+        if (t.TryGetComponent<Rigidbody>(out Rigidbody rb))
+        {
+            _allRigidbodies.Remove(rb);
+        }
+        else
+        {
+            _allTransformsNoRb.Remove(t);
+        }
     }
 }
