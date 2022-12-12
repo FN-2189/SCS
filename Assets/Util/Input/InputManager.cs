89ac76ea16f4cf7824b0d5f3c1f85dca490f3332
Finn <finn294@gmx.de>
2022-12-12 20:19:42 +0100

    add very basic test scene and player movement

--- Assets/Util/Input/InputManager.cs
+++ Assets/Util/Input/InputManager.cs
@@ -23,6 +23,8 @@ public class InputManager : MonoBehaviour
     public static bool Escape { get; private set; }
     public static bool Debug { get; private set; }
     public static Vector2 MouseDelta { get; private set; }
+    public static Vector2 Walk { get; private set; }
+    public static bool Jump { get; private set; }
 
     private float faTriggerCooldown;
 
@@ -81,6 +83,9 @@ public class InputManager : MonoBehaviour
 
         Vector2 delta = input.GeneralControls.Delta.ReadValue<Vector2>();
 
+        Vector2 walk = input.WalkControls.Move.ReadValue<Vector2>();
+        bool jump = input.WalkControls.Jump.ReadValue<float>() > 0f;
+
         Vector3 stick = Vector3.zero;
         stick.x = yawIn;
         stick.y = -pitchAndRollIn.y;
@@ -103,6 +108,9 @@ public class InputManager : MonoBehaviour
         Debug = debug;
 
         MouseDelta = delta * mouseSensitivity;
+
+        Walk = walk;
+        Jump = jump;
 
     }
 }
