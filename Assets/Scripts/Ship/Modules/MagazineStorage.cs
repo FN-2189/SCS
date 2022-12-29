af0668a57591af5d67c86cf60240be49968b7560
Julius <jupster18@gmx.de>
2022-12-29 13:18:44 +0100

    ass Magazine module and blowout particels

---
+++ Assets/Scripts/Ship/Modules/MagazineStorage.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagazineStorage : ShipModule
+{
+    [SerializeField]
+    private GameObject explosionPrefab;
+
+    public int ammoCount;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        base.Update();
+    }
+
+    public override void ModuleDestroyed()
+    {
+        GameObject explosion = Instantiate(explosionPrefab, transform);
+
+        Destroy(explosion, 30f);
+    }
+}
