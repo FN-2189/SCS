fe3f627f99778e8df4ce32c9af80c83b54466680
Rasmus Fey <RasmusFey@outlook.de>
2022-12-29 14:11:27 +0100

    Added edge case handling for SolveQuarticReal

--- Assets/Scripts/Util/Lib/MathHelper.cs
+++ Assets/Scripts/Util/Lib/MathHelper.cs
@@ -16,16 +16,73 @@ public static class MathHelper
         return new double[] { x1, x2 };
     }
 
+    public static bool CloseEqual(double a, double b, double tolerance)
+    {
+        return Math.Abs(a - b) > tolerance;
+    }
+
+    public static Complex[] SolveCubic(double a, double b, double c, double d)
+    {
+        const int NRoots = 3;
+
+        double SquareRootof3 = Math.Sqrt(3);
+        // the 3 cubic roots of 1
+        List<Complex> CubicUnity = new List<Complex>(NRoots)
+                        { new Complex(1, 0), new Complex(-0.5, -SquareRootof3 / 2.0), new Complex(-0.5, SquareRootof3 / 2.0) };
+        // intermediate calculations
+        double DELTA = 18 * a * b * c * d - 4 * b * b * b * d + b * b * c * c - 4 * a * c * c * c - 27 * a * a * d * d;
+        double DELTA0 = b * b - 3 * a * c;
+        double DELTA1 = 2 * b * b * b - 9 * a * b * c + 27 * a * a * d;
+        Complex DELTA2 = -27 * a * a * DELTA;
+        Complex C = Complex.Pow((DELTA1 + Complex.Pow(DELTA2, 0.5)) / 2, 1 / 3.0); //Phew...
+
+        List<Complex> R = new List<Complex>(NRoots);
+        for (int i = 0; i < NRoots; i++)
+        {
+            Complex M = CubicUnity[i] * C;
+            Complex Root = -1.0 / (3 * a) * (b + M + DELTA0 / M);
+            R.Add(Root);
+        }
+        return R.ToArray();
+    }
+
+
     // this is likely the problem
     public static double[] SolveQuarticReal(double a, double b, double c, double d, double e)
     {
-        a += 0.0000000000001;
-        b += 0.0000000000001;
-        c += 0.0000000000001;
-        d += 0.0000000000001;
-        e += 0.0000000000001;
+
+        Complex[] solutions = new Complex[4];
+        List<double> realSolutions = new(4);
+
+        if (CloseEqual(e, 0, 0.001f))
+        {
+            solutions = SolveCubic(a, b, c, d);
+            solutions[3] = 0;
+            foreach (Complex x in solutions)
+            {
+                if (x.Imaginary <= 10e-4) realSolutions.Add(x.Real);
+            }
+
+            return realSolutions.ToArray();
+        }
+
+        if (CloseEqual(a, 0, 0.001f))
+        {
+            solutions = SolveCubic(b, c, d, e);
+            foreach (Complex x in solutions)
+            {
+                if (x.Imaginary <= 10e-4) realSolutions.Add(x.Real);
+            }
+
+            return realSolutions.ToArray();
+        }
+
         double D0 = c * c - 3 * b * d + 12 * a * e;
         double D1 = 2 * c * c * c - 9 * b * c * d + 27 * b * b * e + 27 * a * d * d - 72 * a * c * e;
+        if(CloseEqual(D1, 0, 0.00000001))
+		{
+			throw new ArgumentException("Unhandled division by zero");
+		}
         double p = (8 * a * c - 3 * b * b) / (8 * a * a);
         double q = (b * b * b - 4 * a * b * c + 8 * a * a * d) / (8 * a * a * a);
         Complex Q = Complex.Pow((D1 + Complex.Sqrt(D1 * D1 - 4 * D0 * D0 * D0)) / 2, 1.0 / 3.0);
@@ -37,10 +94,10 @@ public static class MathHelper
         Complex x3 = -b / (4 * a) + S + v;
         Complex x4 = -b / (4 * a) + S - v;
 
-        Complex[] solutions = new Complex[] { x1, x2, x3, x4 };
-        List<double> realSolutions = new(4);
+        solutions = new Complex[] { x1, x2, x3, x4 };
 
-        foreach(Complex x in solutions)
+
+        foreach (Complex x in solutions)
         {
             if (x.Imaginary <= 10e-4) realSolutions.Add(x.Real);
         }
